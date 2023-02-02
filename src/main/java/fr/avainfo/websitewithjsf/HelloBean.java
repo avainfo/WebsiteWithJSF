f5165057ef8e001e0546d1b2cdca8e5730a08d8c
Antonin <antonindosouto@gmail.com>
2023-02-02 23:35:52 +0100

    Initial commit

---
+++ src/main/java/fr/avainfo/websitewithjsf/HelloBean.java
@@ -0,0 +1,32 @@
+package fr.avainfo.websitewithjsf;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+
+@Named(value = "helloBean")
+@RequestScoped
+public class HelloBean {
+
+    private String name;
+    private String message;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void submit() {
+        message = name;
+    }
+}
